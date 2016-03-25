eab03a5bc2d69b7f843377a092c60447a9b484ac
Garrett Minky <gminky019@gmail.com>
2016-03-24 21:31:40 -0700

    added fb stuff YO!

---
+++ Content Media App/Content Media App/FB_Share_BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  FB_Share_BridgingHeader.h
+//  Content Media App
+//
+//  Created by Garrett Minky on 3/24/16.
+//  Copyright © 2016 Garrett Minky. All rights reserved.
+//
+
+#ifndef FB_Share_BridgingHeader_h
+#define FB_Share_BridgingHeader_h
+
+#import<FBSDKShareKit/FBSDKShareKit.h>
+
+#endif /* FB_Share_BridgingHeader_h */
