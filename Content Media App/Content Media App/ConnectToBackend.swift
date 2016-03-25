cd42b04fba47a1552668e159069f2085f522eda5
Lethalaguz <evan.jin@yahoo.com>
2016-03-24 21:18:02 -0700

    Connect to front Connect to back class files started

---
+++ Content Media App/Content Media App/ConnectToBackend.swift
@@ -0,0 +1,9 @@
+//
+//  ConnectToBackend.swift
+//  Content Media App
+//
+//  Created by Evan Jin on 3/24/16.
+//  Copyright © 2016 Garrett Minky. All rights reserved.
+//
+
+import Foundation
---
+++ Content Media App/Content Media App/ConnectToFrontend.swift
@@ -0,0 +1,60 @@
+//
+//  ConnectToFrontend.swift
+//  Content Media App
+//
+//  Created by Evan Jin on 3/24/16.
+//  Copyright © 2016 Garrett Minky. All rights reserved.
+//
+
+import Foundation
+
+class IntegrationFrontend{
+    
+    var DateTime: Int?
+    
+    func getMain() -> MainPageContent{
+        
+        var mainPage = MainPageContent(heros: [Content](), sub: [Content]())
+        
+        return mainPage
+    }
+    
+    func getWatch() -> [Content]{
+        var watch = [Content]()
+        
+        return watch
+    }
+    
+    func getRead() -> [Content]{
+        var read = [Content]()
+        
+        return read
+    }
+    
+    func getShop() -> [Content]{
+        var shop = [Content]()
+        
+        return shop
+    }
+    
+    func getLearn() -> [Content]{
+        var learn = [Content]()
+        
+        return learn
+    }
+    
+    func getSingle(title: String, type: String) -> Content
+    {
+        var single = Content(title: title, description: type)
+        
+        return single
+    }
+    
+    func shareText( txt: String, contentURL: Content ){
+        
+    }
+    
+    func getUpdate( currUpdate: String ){
+    
+    }
+}
