b77280f6f9480a4882c5a4d928c4827620201fcb
Jordan Harris <Hertz381@gmail.com>
2016-03-29 00:09:34 -0700

    Home Page Update

--- Content Media App/Content Media App/ViewController.swift
+++ Content Media App/Content Media App/ViewController.swift
@@ -6,18 +6,195 @@
 //  Copyright © 2016 Garrett Minky. All rights reserved.
 //
 
+import QuartzCore
 import UIKit
 
+/* class ImageViewWithGradient: UIImageView {
+    let myGradientLayer: CAGradientLayer
+    
+    override init(frame:CGRect)
+    {
+        myGradientLayer = CAGradientLayer()
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    required init?(coder aDecoder:NSCoder)
+    {
+        myGradientLayer = CAGradientLayer()
+        super.init(coder: aDecoder)
+        self.setup()
+
+    }
+    
+    func setup()
+    {
+        myGradientLayer.startPoint = CGPoint(x: 0, y: 1)
+        myGradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        let colors:[CGColorRef] = [
+            UIColor.clearColor().CGColor,
+            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor,
+            UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).CGColor,
+            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor,
+            UIColor.clearColor().CGColor ]
+        myGradientLayer.colors = colors
+        myGradientLayer.opaque = false
+        myGradientLayer.locations = [0.0, 0.3, 0.5, 0.7, 1.0]
+    }
+    
+    override func layoutSubviews()
+    {
+        myGradientLayer.frame = self.layer.bounds
+    }
+    
+}
+*/
 class ViewController: UIViewController {
     
     @IBOutlet weak var heroOneView: UIImageView!
+    @IBOutlet weak var heroTwoView: UIImageView!
+    @IBOutlet weak var heroThreeView: UIImageView!
+
+    @IBOutlet weak var heroOneTitle: UITextView!
+    @IBOutlet weak var heroTwoTitle: UITextView!
+    @IBOutlet weak var heroThreeTitle: UITextView!
+    
+    @IBOutlet weak var heroOneType: UITextView!
+    @IBOutlet weak var heroTwoType: UITextView!
+    @IBOutlet weak var heroThreeType: UITextView!
+    
+    @IBOutlet weak var articleOne: UIImageView!
+    @IBOutlet weak var articleTwo: UIImageView!
+    @IBOutlet weak var articleThree: UIImageView!
+    @IBOutlet weak var articleFour: UIImageView!
+    
+    @IBOutlet weak var articleOneType: UITextView!
+    @IBOutlet weak var articleTwoType: UITextView!
+    @IBOutlet weak var articleThreeType: UITextView!
+    @IBOutlet weak var articleFourType: UITextView!
+    
+    @IBOutlet weak var articleOneTitle: UITextView!
+    @IBOutlet weak var articleTwoTitle: UITextView!
+    @IBOutlet weak var articleThreeTitle: UITextView!
+    @IBOutlet weak var articleFourTitle: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        let typeColor = UIColor.init(red: 221/255, green: 180/255, blue: 0/255, alpha: 1)
+        // Hero One
         self.heroOneView.image = UIImage (named: "tempHeroOne.jpg")
         
-        // Do any additional setup after loading the view, typically from a nib.
+        heroOneTitle.text = "Ice Melting In Arctic: How can you save the polar bears?"
+        heroOneTitle.font = UIFont(name: "Roboto-Bold", size: 18)
+        heroOneTitle.textColor = UIColor.whiteColor()
+        
+        heroOneType.text = "READ"
+        heroOneType.font = UIFont(name: "Roboto-Medium", size: 12)
+        heroOneType.textColor = typeColor
+        
+        // Hero Two
+        self.heroTwoView.image = UIImage (named: "tempHeroTwo.jpg")
+        heroTwoTitle.text = "Monkeys of the Jungle"
+        heroTwoTitle.font = UIFont(name: "Roboto-Bold", size: 18)
+        heroTwoTitle.textColor = UIColor.whiteColor()
+        
+        heroTwoType.text = "WATCH"
+        heroTwoType.font = UIFont(name: "Roboto-Medium", size: 12)
+        heroTwoType.textColor = typeColor
+        
+        // Hero Three
+        self.heroThreeView.image = UIImage (named: "tempHeroThree.jpg")
+        heroThreeTitle.text = "Dolphins: Communication with Sonar"
+        heroThreeTitle.font = UIFont(name: "Roboto-Bold", size: 18)
+        heroThreeTitle.textColor = UIColor.whiteColor()
+        
+        heroThreeType.text = "LEARN"
+        heroThreeType.font = UIFont(name: "Roboto-Medium", size: 12)
+        heroThreeType.textColor = typeColor
+        
+        
+        // Add Gradient to all heroes
+        let gradientLayerH1 = CAGradientLayer.init()
+        let gradientLayerH2 = CAGradientLayer.init()
+        let gradientLayerH3 = CAGradientLayer.init()
+        
+        let colors = [ UIColor.init(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0.7).CGColor,
+            UIColor.init(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0).CGColor ]
+        let startPoint = CGPointMake(0.0, 1.0)
+        let endPoint = CGPointMake(0.0, 0.0)
+        
+        gradientLayerH1.frame = heroOneView.bounds
+        gradientLayerH2.frame = heroTwoView.bounds
+        gradientLayerH3.frame = heroThreeView.bounds
+        
+        
+        gradientLayerH1.colors = colors
+        gradientLayerH2.colors = colors
+        gradientLayerH3.colors = colors
+        
+        gradientLayerH1.startPoint = startPoint
+        gradientLayerH1.endPoint = endPoint
+        gradientLayerH2.startPoint = startPoint
+        gradientLayerH2.endPoint = endPoint
+        gradientLayerH3.startPoint = startPoint
+        gradientLayerH3.endPoint = endPoint
+        
+        self.heroOneView.layer.addSublayer(gradientLayerH1)
+        self.heroTwoView.layer.addSublayer(gradientLayerH2)
+        self.heroThreeView.layer.addSublayer(gradientLayerH3)
+        
+        // Article One
+        self.articleOne.image = UIImage (named: "tempArtOne.jpg")
+        
+        articleOneType.text = "READ"
+        articleOneType.font = UIFont(name: "Roboto-Medium", size: 12)
+        articleOneType.textColor = typeColor
+        articleOneType.textAlignment = .Center
+        
+        articleOneTitle.text = "Jungle Book: Out Now, and it's good!"
+        articleOneTitle.font = UIFont(name: "Roboto-Bold", size: 14)
+        articleOneTitle.textColor = UIColor.blackColor()
+        
+        // Article Two
+        self.articleTwo.image = UIImage (named: "tempArtTwo.jpg")
+        
+        articleTwoType.text = "LEARN"
+        articleTwoType.font = UIFont(name: "Roboto-Medium", size: 12)
+        articleTwoType.textColor = typeColor
+        articleTwoType.textAlignment = .Center
+        
+        
+        articleTwoTitle.text = "The Incredibly Diverse Ecosystem in Just This River Will Blow Your Mind"
+        articleTwoTitle.font = UIFont(name: "Roboto-Bold", size: 14)
+        articleTwoTitle.textColor = UIColor.blackColor()
+        
+        // Article Three
+        self.articleThree.image = UIImage (named: "BB8.jpg")
+        
+        articleThreeType.text = "WATCH"
+        articleThreeType.font = UIFont(name: "Roboto-Medium", size: 12)
+        articleThreeType.textColor = typeColor
+        articleThreeType.textAlignment = .Center
+        
+        
+        articleThreeTitle.text = "BB8 Saves The Day"
+        articleThreeTitle.font = UIFont(name: "Roboto-Bold", size: 14)
+        articleThreeTitle.textColor = UIColor.blackColor()
+        
+        // Article Four
+        self.articleFour.image = UIImage (named: "KyloRen.png")
+        
+        articleFourType.text = "LEARN"
+        articleFourType.font = UIFont(name: "Roboto-Medium", size: 12)
+        articleFourType.textColor = typeColor
+        articleFourType.textAlignment = .Center
+        
+        
+        articleFourTitle.text = "Learn The Dark Side of The Force with Kylo Ren"
+        articleFourTitle.font = UIFont(name: "Roboto-Bold", size: 14)
+        articleFourTitle.textColor = UIColor.blackColor()
     }
     
     override func viewDidAppear(animated: Bool) {
